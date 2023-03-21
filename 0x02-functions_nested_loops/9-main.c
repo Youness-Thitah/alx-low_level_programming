084132662190157a560e968575b26072e8a3d83e
Youness-Thitah <thitahyouness@gmail.com>
2023-03-21 16:22:42 -0700

    Task 9

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * main - check the code
 *
@@ -7,8 +6,6 @@
 */
 int main(void)
 {
-int n;
-n = add(89, 9);
-printf("%d\n", n);
+times_table();
 return (0);
 }
